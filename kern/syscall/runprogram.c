5521744aff9a77ea249bbde88011d6b7743eddd0
Pratham Malik <trinity@zion.(none)>
2015-05-24 01:40:34 -0400

    Updated runprogram.c

--- kern/syscall/runprogram.c
+++ kern/syscall/runprogram.c
@@ -129,62 +129,15 @@ runprogram(char *progname)
 		return result;
 	}
 
-		/*while(args[count] != NULL){
-			//karray[count];
-			k_count++;
-			int string_length = strlen(args[count])+1;
-				int new_length = string_length;
-				if((string_length) % 4 != 0)
-				{
-					while(new_length%4 !=0)
-					{
-						new_length++;
-					}
-					for(int i=string_length;i<=new_length;i++)
-					{
-						args[count][i]= '\0';
-					}
-				}
-			//char *k_des= kmalloc(sizeof(char*));
-			size_t final_length= (size_t)new_length;
-			//size_t actual_length;
-			if((result=copyinstr((const_userptr_t)args[count], k_des, sizeof(args[count]), &actual_length ))!= 0){
-					kfree(k_des);
-					return result;
-			}
-			if(count==0){
-				final_stack= stackptr- final_length;
-			}
-			else{
-				final_stack= (size_t)karray[k_count-1]- final_length;
-			//}
-			size_t actual_length1;
-			result= copyoutstr(args[count], (userptr_t) (final_stack), final_length, &actual_length1);
-			if(result){
-				return result;
-			}
-
-			karray[k_count]=  (char*)(final_stack);
-
-			count++;
-		}*/
-
-		//size_t actual_length;
-
 	karray[k_count+1]=  (char*)NULL;
 	int value= k_count+1;
 	int arr_length = (value+1)*sizeof(char*);
 	final_stack= (size_t)karray[k_count]- arr_length;
-	/*karray[count]=  (char*)(final_stack+null_length+ pointer_length);*/
 
-		//char *x= &karray[0];
-		result= copyout(karray, (userptr_t) (final_stack),arr_length);
-		if(result){
-			return result;
-		}
-
-
-
+	result= copyout(karray, (userptr_t) (final_stack),arr_length);
+	if(result){
+		return result;
+	}
 
 	int result1=100;
 	//kprintf("Inside run program");
@@ -193,21 +146,6 @@ runprogram(char *progname)
 		kprintf("Error");
 		return result1;
 	}
-
-	/*
-	* Ended
-	*/
-
-
-
-/*
-//End of Additions by MA
-	result = as_define_stack(curthread->t_addrspace, &stackptr);
-		if (result) {
-			 thread_exit destroys curthread->t_addrspace
-			return result;
-		}
-*/
 
 	/* Warp to user mode. */
 	enter_new_process(1 /*argc*/, (userptr_t)(final_stack) /*userspace addr of argv*/,
